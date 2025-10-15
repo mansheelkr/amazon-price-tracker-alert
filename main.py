51be5af233d8057caf23542d8ee92aa0ab309330
Mansheel Randhawa <mansheelrandhawa2001@gmail.com>
2025-10-15 19:05:18 -0400

    Moved header credentials to .env file

--- main.py
+++ main.py
@@ -8,21 +8,8 @@ load_dotenv()
 
 #practice_URL = "https://appbrewery.github.io/instant_pot/"
 live_URL = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1"
-#header = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"}
-header = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Cookie": '__gads=ID=7b6971f347d8bd41:T=1760565361:RT=1760565361:S=ALNI_MZT71-Ma-GPqm4ytzeXwSWdqPup4Q; __gpi=UID=0000129ed3cd0793:T=1760565361:RT=1760565361:S=ALNI_MbIpUGiQ6YdGuPcnSXc1TaSEAa-7w; __eoi=ID=2299d82db7c60040:T=1760565361:RT=1760565361:S=AA-Afja1FvHPL-aSNSf5mLUpqBx5; FCNEC=%5B%5B%22AKsRol_XwkfnMega-PuQvhAV7kZeU3dP52U3QxrHTpN4AtmaBu25pU51h8D7qu7Xe8KIBmd3z6VXmOza8s3FCIuf4mH9SsTgDIZmQpe_h1TH4AJUAnTFeFF2VstYEKY-vpShrZqZ0WCGXraUsVMdaR1KsMX6d2gLAQ%3D%3D%22%5D%5D',
-    "Priority": "u=0, i",
-    "Referer": live_URL,
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "none",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
-}
 
-response = requests.get(url=live_URL, headers=header)
+response = requests.get(url=live_URL, headers=os.getenv("header"))
 amazon_webpage = response.content
 
 soup = BeautifulSoup(amazon_webpage, "html.parser")
